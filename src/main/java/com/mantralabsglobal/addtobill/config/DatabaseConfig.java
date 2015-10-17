47b28b87d80aaf7dfc75f68024190e02c1d61167
pravk <hello.praveen@gmail.com>
2015-10-17 18:02:18 +0530

    Reading DB from Environment

--- src/main/java/com/mantralabsglobal/addtobill/config/DatabaseConfig.java
+++ src/main/java/com/mantralabsglobal/addtobill/config/DatabaseConfig.java
@@ -2,6 +2,7 @@ package com.mantralabsglobal.addtobill.config;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
@@ -13,13 +14,26 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 @EnableMongoRepositories(basePackages="com.mantralabsglobal.addtobill")
 public class DatabaseConfig {
 
+	@Value(value="${spring.datasource.url}")
+	private String sqlUrl;
+	
+	@Value(value="${spring.datasource.username}")
+	private String userName;
+
+	@Value(value="${spring.datasource.password}")
+	private String password;
+
+	@Value(value="${spring.datasource.driver-class-name}")
+	private String driver;
+
+	
 	@Bean(name="lockDS")
     DataSource dsDB1() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl("jdbc:mysql://localhost:3306");
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl(sqlUrl);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driver);
         return dataSource;
     }
 	
