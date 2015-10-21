a006a69143b49f74f68ae473fba578e7d1d71be6
pravk <hello.praveen@gmail.com>
2015-10-21 23:30:12 +0530

    Logging exceptions

--- src/main/java/com/mantralabsglobal/addtobill/controllers/BaseController.java
+++ src/main/java/com/mantralabsglobal/addtobill/controllers/BaseController.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 public class BaseController {
 
+	private static Logger logger = LoggerFactory.getLogger(BaseController.class);
+	
 	@ExceptionHandler(IllegalArgumentException.class)
 	public ResponseEntity<ErrorResponse> handleException(HttpServletRequest req, IllegalArgumentException exception) {
+		logger.warn("Request failed", exception);
 		ErrorResponse response = new ErrorResponse();
 		response.setMessage(exception.getMessage());
 		response.setPath(req.getServletPath());
@@ -24,6 +29,7 @@ public class BaseController {
 	
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorResponse> handleException(HttpServletRequest req, Exception exception) {
+		logger.warn("Request failed", exception);
 		ErrorResponse response = new ErrorResponse();
 		//response.setMessage(exception.getMessage());
 		response.setPath(req.getServletPath());
